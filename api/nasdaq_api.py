d8fc2409390c192558d1bcc7659bfa4634557a96
= <=>
2025-05-17 10:49:14 -0700

    Back to functional. Set types of AI and DB output

--- api/nasdaq_api.py
+++ api/nasdaq_api.py
@@ -11,24 +11,30 @@ def add_greeks_to_contract(index: int, greeks_dict: dict, prices_dict: dict, tic
         ticker.df_contracts.at[index, col] = value
 
 def get_greeks_url(ticker, prediction: dict, contract: pd.DataFrame) -> str | bool:
-    if ((prediction.prediction >= contract.last_trade) and
+    if ((contract.prediction >= contract.last_trade) and
         contract.call_put == "P" and
         contract.expiry_date == prediction.expiry_date):
             return f"https://api.nasdaq.com/api/quote/{ticker.ticker}/option-chain?assetclass=stocks&recordID={contract.nasdaq_ticker}"
-    elif ((prediction < contract.last_trade) and
+    elif ((contract.prediction < contract.last_trade) and
         contract.call_put == "C" and
         contract.expiry_date == prediction.expiry_date):
             return f"https://api.nasdaq.com/api/quote/{ticker}/option-chain?assetclass=stocks&recordID={contract.nasdaq_ticker}"
     else: return False
 
 def get_greeks(ticker, prediction: dict):
-    for index, contract in tqdm(ticker.df_contracts.iterrows(), total=len(ticker.df_contracts), desc=f"Fetching Greeks for {ticker}"):
-        url_greeks = get_greeks_url(ticker, prediction, contract)
-        if url_greeks:
+    for index, contract in tqdm(
+        ticker.df_contracts.iterrows(),
+        total=len(ticker.df_contracts),
+        desc=f"Fetching Greeks for {ticker.ticker}"
+    ):
+        try:
+            url_greeks = get_greeks_url(ticker, prediction, contract)
+            if not url_greeks: continue
             data = fetch_data(url_greeks)
-        if data:
             greeks_dict, prices_dict = parse_greeks_prices(contract, data)
             add_greeks_to_contract(index, greeks_dict, prices_dict, ticker, contract)
+        except:
+            continue
 
 def get_price_history(ticker):
     today = datetime.now().strftime("%Y-%m-%d")[:10]
--- utils/conversions.py
+++ utils/conversions.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def convert_float(value: str):
     try:
         return float(value)
@@ -9,3 +11,9 @@ def clean_prices(response: str) -> list[float]:
     for price in prices:
         price = convert_float(price)
     return prices
+
+def convert_df_to_float(df):
+    numeric_cols = ["current_price", "prediction", "last_trade", "delta", "gamma", "rho", "theta", "vega", "imp_vol", "bid", "ask"]
+    for col in numeric_cols:
+        if col in df.columns:
+            df[col] = pd.to_numeric(df[col], errors="coerce")
