8aa4675b878482a21fd5e59bc8b7855ce0bba759
= <=>
2025-06-14 13:12:16 -0700

    mongo commit

--- api/mongo_api.py
+++ api/mongo_api.py
@@ -18,3 +18,48 @@ def add_db_items(db, records):
         db.predictions.insert_many(records)
     except:
         print("Failed to post to DB.")
+
+def add_db_item(db, record):
+    try:
+        db.predictions.insert_one(record)
+    except:
+        print("Failed to post to DB.")
+
+def average_duplicates():
+    ticker = "AAPL"    
+    db = connect_to_db()
+    predictions = db["predictions"]
+    docs = list(predictions.find( { "ticker": ticker } ))
+    df_docs = pd.DataFrame(docs)
+    expiries = {}
+    
+    for date in df_docs.expiry_date.unique():
+        expiries[date] = []
+    for index, doc in df_docs.iterrows():
+        expiries[doc.expiry_date].append(doc)
+    duplicates = []
+    for date in expiries:
+        if len(expiries[date]) > 1:
+            duplicate = {
+                    "date": date,
+                    "prices": []
+                }
+            for doc in expiries[date]:
+                duplicate["prices"].append(float(doc["prediction"]))
+            duplicates.append(duplicate)
+
+    for duplicate in duplicates:
+        duplicate["prices"] = sum(duplicate["prices"]) / len(duplicate["prices"])
+        db.predictions.delete_many( { "ticker": ticker, "expiry_date": duplicate["date"]})
+        record = {
+            "date": "2025-05-17",
+            "current_price": 211.26,
+            "ticker": ticker,
+            "expiry_date": duplicate[""],
+            "prediction": duplicate["rpices"]
+        }
+    
+
+    add_db_item(db, record)
+
+# average_duplicates()
